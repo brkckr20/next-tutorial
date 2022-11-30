daabca0869b28d87c13eb8e71870595930db963d
Burak Çakır <burak2043@gmail.com>
2022-11-30 14:24:45 +0300

    contact started

---
+++ portfolio-next/components/Contact.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { AiOutlineMail } from 'react-icons/ai'
+import { BsFillPersonBadgeFill } from 'react-icons/bs'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+
+const Contact = () => {
+    return (
+        <div className='w-full lg:h-screen'>
+            <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
+                <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Contact</p>
+                <h2 className='py-4'>Get In Touch</h2>
+                <div className='grid lg:grid-cols-5 gap-8'>
+                    {/* left */}
+                    <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
+                        <div className='lg:p-4 h-full'>
+                            <div>
+                                <img className='rounded-xl hover:scale-105 ease-in duration-300' src="https://images.unsplash.com/photo-1508984921474-bc8302cb4d04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
+                            </div>
+                            <div>
+                                <h2 className='py-2'>Name here</h2>
+                                <p>Front-end Developer</p>
+                                <p className='py-4'>I am avaliable for freelance or full-time position. Contact me and let's talk.</p>
+                            </div>
+
+                            <div className='uppercase'>
+                                <p>Connect With Me!</p>
+                                <div className='flex items-center justify-between py-4'>
+
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <FaLinkedinIn />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <FaGithub />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <AiOutlineMail />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                                        <BsFillPersonBadgeFill />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
