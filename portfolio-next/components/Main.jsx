98c54c832a059365fdc83f4c61a7149d97410975
Burak Çakır <burak2043@gmail.com>
2022-12-06 14:54:25 +0300

    smoot scrool added

--- portfolio-next/components/Main.jsx
+++ portfolio-next/components/Main.jsx
@@ -5,7 +5,7 @@ import {FaGithub, FaLinkedinIn } from 'react-icons/fa'
 
 const Main = () => {
     return (
-        <div className='w-full h-screen text-center'>
+        <div id='home' className='w-full h-screen text-center'>
             <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex items-center justify-center'>
                 <div className=''>
                     <p className='uppercase text-sm tracking-widest text-gray-600'>LET'S BUILD SOMETHING TOGETHER</p>
