bf6721092c2bfa6c44f805fa038242914ee2c76a
Burak Çakır <burak2043@gmail.com>
2022-12-09 17:07:50 +0300

    property image added

---
+++ portfolio-next/README.md
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=CMx51wpd7g4&t=7435s
--- portfolio-next/pages/property.jsx
+++ portfolio-next/pages/property.jsx
@@ -1,10 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
 const property = () => {
     return (
         <div className='w-full'>
-            <div className='w-screen h-[30vh] lg:w-[40vh] relative'>
-                asdad
+            <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
+                <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10'>
+                    <img className='absolute z-10' objectFit='cover' layout='fill'
+                        src="https://images.unsplash.com/photo-1657299142018-4f7f33aea18c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                        alt="/" />
+                </div>
             </div>
         </div>
     )
