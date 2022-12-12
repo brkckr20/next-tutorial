4b3194469e63df343c94d2087883d21016bea4f1
Burak Çakır <burak2043@gmail.com>
2022-12-12 15:43:13 +0300

    property edited

--- portfolio-next/pages/property.jsx
+++ portfolio-next/pages/property.jsx
@@ -1,14 +1,19 @@
 import Image from 'next/image'
-import React from 'react'
+import React from 'react';
+import propertyImage from '../public/assets/property.jpg';
 
 const property = () => {
     return (
         <div className='w-full'>
-            <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
-                <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10'>
-                    <img className='absolute z-10' objectFit='cover' layout='fill'
-                        src="https://images.unsplash.com/photo-1657299142018-4f7f33aea18c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            <div className='w-screen h-[50vh] relative'>
+                <div className='absolute top-0 left-0 w-full h-[50vh]  bg-black/70 z-10'>
+                    <Image className='absolute z-10' objectFit='cover' layout='fill'
+                        src={propertyImage}
                         alt="/" />
+                    <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] '>
+                        <h2 className='py-2 text-black'>Property</h2>
+                        <h3>React JS / Tailwind / Firebase</h3>
+                    </div>
                 </div>
             </div>
         </div>
